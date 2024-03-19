f6212aa55c95fda3d120294a16c036d274512283
Nikitakumarisharma <113896209+Nikitakumarisharma@users.noreply.github.com>
2024-03-19 20:46:46 +0530

    about added

--- src/myComponent/about.js
+++ src/myComponent/about.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+            Its About TODO Website
+            <p>A todo list website simplifies task management by allowing users to create, organize, and prioritize tasks online. Users can add, edit, and delete items, set deadlines, and categorize tasks for better organization. With features like reminders and notifications, users stay on top of their commitments. Collaborative functionalities enable teams to share lists and work collectively towards goals. Intuitive interfaces and customizable options enhance user experience, making it easy to navigate and use. Whether for personal productivity or team projects, todo list websites streamline workflow and boost efficiency, ensuring tasks are completed on time and goals are achieved effectively.</p>
         </div>
     );
 }
